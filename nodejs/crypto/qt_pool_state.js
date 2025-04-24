54ef1e3c455e2acf8ab8ac70f102ef1b295da62a
roman <kosha9@mail.ru>
2025-04-24 17:08:40 +0300

    made qt_pool_swap.js script

---
+++ nodejs/crypto/qt_pool_state.js
@@ -0,0 +1,109 @@
+
+//скрипт возвращает текущее состояние пула, номер тика и цены активов относительно друг друга token0/token1
+//ARG_1
+// один обязательный аргумент - json file со следующими полями:
+//EXAMPLE
+/*
+{
+    "address": "0x2aceda63b5e958c45bd27d916ba701bc1dc08f7a",
+    "fee": 3000,
+    "tick_space": 60,
+    "token0": "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
+    "token1": "0xd6df932a45c0f255f85145f286ea0b292b21c90b"
+}
+*/
+
+
+
+//including
+const m_base = require("./base.js");
+const {space, log, curTime, delay, isInt, varNumber, jsonFromFile, isJson, jsonKeys, hasField} = require("./utils.js");
+const m_wallet = require("./obj_wallet.js");
+const {ArgsParser} = require("./obj_argsparser.js");
+const m_pool = require("./obj_pool.js");
+
+
+// user vars
+let result = {type: "state"};
+const sendResult = () => log("JSON_RESULT_START", result, "JSON_RESULT_END");
+function sendErrResult(err) {result.error = err; sendResult();}    
+
+
+//read input args
+let a_parser = new ArgsParser(process.argv);
+if (a_parser.isEmpty()) {sendErrResult("invalid args (is empty)"); return;}
+if (a_parser.count() != 1) {sendErrResult("invalid args (parameters != 1)"); return;}
+
+
+//read args
+const F_JSON = a_parser.first();
+log("JSON-file: ", F_JSON);
+const j_params  = jsonFromFile(F_JSON);
+if (!isJson(j_params)) {sendErrResult("can't readed JSON data"); return;}
+log("JSON parsed OK! \n\n DATA:", j_params);
+
+//check filelds kit
+if (!hasField(j_params, "address")) {sendErrResult("json-params has not field: [address]"); return;}
+if (!hasField(j_params, "fee")) {sendErrResult("json-params has not field: [fee]"); return;}
+if (!hasField(j_params, "tick_space")) {sendErrResult("json-params has not field: [tick_space]"); return;}
+if (!hasField(j_params, "token0")) {sendErrResult("json-params has not field: [token0]"); return;}
+if (!hasField(j_params, "token1")) {sendErrResult("json-params has not field: [token1]"); return;}
+log("JSON fields kit OK! ");
+space();
+
+
+//body
+let p_obj = new m_pool.PoolObj(j_params.address);
+p_obj.fee = j_params.fee;
+p_obj.tick_space = j_params.tick_space;
+
+
+function getAssetAttrs(t_index, t_addr, w_obj)
+{
+    const asset = w_obj.findAsset(t_addr);
+    if (t_index == 0)
+    {
+	p_obj.T0.address = t_addr;
+	p_obj.T0.decimal = asset.decimal;
+	p_obj.T0.ticker = asset.ticker;
+    }
+    else
+    {
+	p_obj.T1.address = t_addr;
+	p_obj.T1.decimal = asset.decimal;
+	p_obj.T1.ticker = asset.ticker;
+    }
+}
+
+//WALLET DATA
+let w_obj = new m_wallet.WalletObj(process.env.WA2);
+getAssetAttrs(0, j_params.token0, w_obj);
+getAssetAttrs(1, j_params.token1, w_obj);
+//w_obj.outAssets();
+//p_obj.out();
+
+result.pool_address = j_params.address;
+try
+{
+
+    p_obj.updateState().then(() => {
+	p_obj.out();
+	p_obj.showPrices();
+
+	space();
+	p_obj.outState();
+
+	space();
+	result.tick = p_obj.state.tick;
+	result.price0 = p_obj.T0.price.toFixed(8);
+	result.price1 = p_obj.T1.price.toFixed(8);
+	sendResult();
+    });
+
+}
+catch(e) 
+{
+    const err = ("ERROR: " + e);
+    sendErrResult(err);
+}
+
---
+++ nodejs/crypto/qt_pool_swap.js
@@ -0,0 +1,124 @@
+
+//скрипт производит обмен токенов token0/token1 между собой, возвращает результат обмена.
+//если поле 'simulate_mode' задать 'yes', скрипт выполнит обмен в режиме эмуляции, при этом можно посмотреть сколько токенов будет на выходе
+
+//ARG_1
+// один обязательный аргумент - json file со следующими полями:
+//EXAMPLE
+/*
+{
+    "pool_address": "0x2aceda63b5e958c45bd27d916ba701bc1dc08f7a",
+    "input_token": 0,  // index of pair pool assets, maybe 0/1
+    "dead_line": 120, //seconds
+    "size": 16.5,
+    "simulate_mode": "yes", // maybe yes/no
+
+}
+*/
+
+
+
+//including
+const m_base = require("./base.js");
+const {space, log, curTime, delay, isInt, varNumber, jsonFromFile, isJson, jsonKeys, hasField} = require("./utils.js");
+const m_wallet = require("./obj_wallet.js");
+const {ArgsParser} = require("./obj_argsparser.js");
+const m_pool = require("./obj_pool.js");
+const m_swapper = require("./obj_swapper.js");
+
+//константы для определения размера газа перед совершением транзакции
+const GAS_LIMIT = 365000; //единиц газа за транзакцию
+const MAX_FEE = 280;  //Gweis
+const PRIOR_FEE = -1;  //Gweis
+
+
+// user vars
+let result = {type: "swap"};
+const sendResult = () => log("JSON_RESULT_START", result, "JSON_RESULT_END");
+function sendErrResult(err) {result.error = err; sendResult();}    
+
+
+//read input args
+let a_parser = new ArgsParser(process.argv);
+if (a_parser.isEmpty()) {sendErrResult("invalid args (is empty)"); return;}
+if (a_parser.count() != 1) {sendErrResult("invalid args (parameters != 1)"); return;}
+
+
+//read args
+const F_JSON = a_parser.first();
+log("JSON-file: ", F_JSON);
+const j_params  = jsonFromFile(F_JSON);
+if (!isJson(j_params)) {sendErrResult("can't readed JSON data"); return;}
+log("JSON parsed OK! \n\n DATA:", j_params);
+
+//check filelds kit
+if (!hasField(j_params, "pool_address")) {sendErrResult("json-params has not field: [pool_address]"); return;}
+if (!hasField(j_params, "input_token")) {sendErrResult("json-params has not field: [input_token]"); return;}
+if (!hasField(j_params, "dead_line")) {sendErrResult("json-params has not field: [dead_line]"); return;}
+if (!hasField(j_params, "size")) {sendErrResult("json-params has not field: [size]"); return;}
+if (!hasField(j_params, "simulate_mode")) {sendErrResult("json-params has not field: [simulate_mode]"); return;}
+log("JSON fields kit OK! ");
+space();
+
+
+//user const
+const POOL_ADDR = j_params.pool_address;
+const IS_SUMULATE = ((j_params.simulate_mode == "no") ? false : true);
+const DEAD_LINE = Number.parseInt(j_params.dead_line);
+const INPUT_TOKEN = Number.parseInt(j_params.input_token);
+const INPUT_SUM = Number.parseFloat(j_params.size); // amount of input token
+log("Input params OK!");
+log("POOL_ADDR:", POOL_ADDR);
+log("IS_SUMULATE:", IS_SUMULATE);
+log("DEAD_LINE:", DEAD_LINE);
+log("INPUT_TOKEN:", INPUT_TOKEN);
+log("INPUT_SUM:", INPUT_SUM);
+
+
+
+
+
+//body
+log("//////////////////WITH SWAPPER_OBJ/////////////////////////");
+let w_obj = new m_wallet.WalletObj(process.env.WA2, process.env.WKEY);
+w_obj.setGas(GAS_LIMIT, MAX_FEE, PRIOR_FEE);
+
+let s_obj = new m_swapper.SwapperObj(w_obj, POOL_ADDR);
+s_obj.setSimulateMode(IS_SUMULATE); //TURN ON/OFF SIMULATE_MODE
+
+result.pool_address = j_params.pool_address;
+result.simulate_mode = j_params.simulate_mode;
+result.input_token = j_params.input_token;
+result.input_size = j_params.size;
+
+if (IS_SUMULATE)
+{
+    try
+    {
+	s_obj.tokenSizeBySwapping(INPUT_SUM, INPUT_TOKEN).then((out) => {
+	    log("out_size = ", out);    
+	    result.output_size = out;
+	    sendResult();
+	});
+    }
+    catch(e) {const err = ("ERROR: " + e); sendErrResult(err);}
+}
+else
+{
+    try
+    {
+	s_obj.trySwap(INPUT_SUM, INPUT_TOKEN, DEAD_LINE).then((data) => {
+    	    log("result: ", data)
+    	    if (data.code == true) //send TX Ok!
+    	    {
+        	result.result_code = "OK";
+        	result.tx_hash = data.tx_hash;
+    	    }
+    	    else result.result_code = "FAULT";
+    	    space();
+    	    sendResult();
+	});
+    }
+    catch(e) {const err = ("ERROR: " + e); sendErrResult(err);}
+}
+
